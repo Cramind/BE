3e4230491919502b75a8ff86bc1e28e48098c9bf
Jinyoung <glitt5384@naver.com>
2025-08-23 23:43:29 +0900

    feat: 팀 관련 Api 요청을 받는 컨트롤러

---
+++ src/main/java/com/groupstudy/domain/team/controller/TeamController.java
@@ -0,0 +1,25 @@
+package com.groupstudy.domain.team.controller;
+
+import com.groupstudy.domain.team.dto.TeamCreateDto;
+import com.groupstudy.domain.team.service.TeamCommandService;
+import com.groupstudy.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/team")
+@RequiredArgsConstructor
+public class TeamController {
+    private final TeamCommandService teamCommandService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> postNewTeam(@RequestBody TeamCreateDto teamCreateDto){
+        teamCommandService.insertNewTeam(teamCreateDto);
+        return ResponseEntity.ok(ApiResponse.onSuccessVoid());
+    }
+
+}
