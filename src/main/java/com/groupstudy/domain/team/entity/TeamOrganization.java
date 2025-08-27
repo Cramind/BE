8cdbe6d2c66af732749bec156ba35a774feb84d5
Jinyoung <glitt5384@naver.com>
2025-08-27 15:56:58 +0900

    feat: 팀 레포지토리 배열 필드 추가

--- src/main/java/com/groupstudy/domain/team/entity/TeamOrganization.java
+++ src/main/java/com/groupstudy/domain/team/entity/TeamOrganization.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "team_organization")
 @Getter
@@ -14,11 +16,15 @@ import lombok.NoArgsConstructor;
 public class TeamOrganization extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, name = "team_org_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team parentTeam;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "parentTeamOrg", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TeamRepo> childrenRepos;
 
     private String orgName;
 
