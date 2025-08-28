8ed40f3a91f3978db96c3029b5a442dadcff344f
Jinyoung <glitt5384@naver.com>
2025-08-28 23:27:32 +0900

    feat: 팀 초대 정보 Dto를 생성하는 비즈니스 로직 추가

--- src/main/java/com/groupstudy/domain/team/service/TeamCommandService.java
+++ src/main/java/com/groupstudy/domain/team/service/TeamCommandService.java
@@ -1,8 +1,11 @@
 package com.groupstudy.domain.team.service;
 
 import com.groupstudy.domain.team.dto.TeamCreateDto;
+import com.groupstudy.domain.team.dto.response.TeamInvitationInfoResponse;
+import com.groupstudy.domain.teammember.dto.response.TeamMemberInfoResponse;
 import com.groupstudy.global.auth.CustomUserDetails;
 
 public interface TeamCommandService {
     void insertNewTeam(CustomUserDetails customUserDetails, TeamCreateDto creationDto);
+    TeamInvitationInfoResponse fetchTeamMetaData(String inviteCode);
 }
