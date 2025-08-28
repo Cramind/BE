f63e7e2d0d23c24a0926b4873293009691777999
Jinyoung <glitt5384@naver.com>
2025-08-28 23:28:05 +0900

    feat: 코드값으로 코드 객체 검색

--- src/main/java/com/groupstudy/domain/team/repository/CustomInviteCodeRepository.java
+++ src/main/java/com/groupstudy/domain/team/repository/CustomInviteCodeRepository.java
@@ -1,6 +1,13 @@
 package com.groupstudy.domain.team.repository;
 
+import com.groupstudy.domain.team.entity.InviteCode;
+import com.groupstudy.domain.user.entity.User;
+
+import java.util.Optional;
+
 public interface CustomInviteCodeRepository {
     String findByStudyRoomId(Long studyroomId);
     Long findStudyRoomByCode(String code);
+    Optional<InviteCode> findByCode(String code);
+
 }
